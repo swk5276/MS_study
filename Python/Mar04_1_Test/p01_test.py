5c53f033ad7a76d1be2f693b06fba8f502d39def
김성웅 <swk5276@gmail.com>
2025-03-07 15:27:07 +0900

    Ms_study_0307

---
+++ Python/Mar04_1_Test/p01_test.py
@@ -0,0 +1,7 @@
+from http.client import HTTPConnection
+from time import sleep
+
+
+print("ㅋ")
+HTTPConnection
+sleep
---
+++ Python/Mar04_1_Test/p02_test2.py
@@ -0,0 +1,37 @@
+from time import sleep
+# python : 프로그래밍 언어
+# ㅎㅎ출력하는 프로그램 => 프로그램 개발 끝?
+# 실행할 수 있도록 서버 컴에서 실행
+
+# 웹에서 훈련용 데이터를 매일 아침 6시에 받아 DB에 저장하는 프로그램 (에저에서 실행)
+# 서버 python 실행 : python 파일명
+
+### 실행 파일 만들기(windows 버전) ###
+# windows python 실행 : 
+# dos 명령어 기반
+# .bat : 명령어를 적어 놓은 파일 (window용 / python 필요)
+# .exe : (python 필요 X)
+print("ㅎㅎ")
+sleep(5)
+
+
+### 실행 파일 만들기(python 버전) ###
+# 개발자들이 만든 거를 공유하는 문화 => 공유 문화의 중앙제어 시스템 => pip
+
+# pyinstaller : 실행파일 만들어주는 pip
+# pip install pyinstaller
+# 실행파일 만들기
+# pyinstaller 파일명
+#   pyinstaller p02_test2.py
+#   dist\p02_test2\p02_test2.exe
+
+# IOS 앱 : objective-C
+# android 앱 : Java (전세계적)
+# 프로그래밍 언어를 마스터?
+# 조건문 # 반복문 # 컬랙션 
+# 그 언어만의 특징 : java-python 개발자 많음 [남이 만든거 잘 쓰자]
+# 개발자들이 만든 거를 공유하는 문화 => 공유 문화의 중앙제어 시스템 => pip
+
+# 빅데이터 분석 훈련
+# 10tb 파일 pc 저장 x => 서버급 컴 사용 (분산 저장)
+# linux, DB
